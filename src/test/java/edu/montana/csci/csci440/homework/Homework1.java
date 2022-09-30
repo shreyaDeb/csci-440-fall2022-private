1dbfa162ad0ee2f7bc2f283d323284004efb1fa7
shreyaDeb <shreya.deb@icloud.com>
2022-09-30 08:21:28 -0600

    1/3

--- src/test/java/edu/montana/csci/csci440/homework/Homework1.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework1.java
@@ -14,7 +14,7 @@ public class Homework1 extends DBTest {
      * Write a query in the string below that returns all artists that have an 'A' in their name
      */
     void selectArtistsWhoseNameHasAnAInIt(){
-        List<Map<String, Object>> results = executeSQL("SELECT * FROM artists");
+        List<Map<String, Object>> results = executeSQL("SELECT * FROM artists WHERE name LIKE '%A%' ");
         assertEquals(211, results.size());
     }
 
@@ -24,7 +24,7 @@ public class Homework1 extends DBTest {
      */
     void selectAllArtistsWithMoreThanOneAlbum(){
         List<Map<String, Object>> results = executeSQL(
-                "SELECT * FROM artists");
+                "SELECT * FROM artists WHERE albumTitle  ");
 
         assertEquals(56, results.size());
         assertEquals("AC/DC", results.get(0).get("Name"));
@@ -37,8 +37,8 @@ public class Homework1 extends DBTest {
          */
     void selectTheTrackAndAlbumAndArtistForAllTracksLongerThanSixMinutes() {
         List<Map<String, Object>> results = executeSQL(
-                "SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName FROM tracks " +
-                        "-- NEED TO DO SOME JOINS HERE KIDS");
+                "SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName FROM tracks WHERE Millisecond > 6*60*10000" +
+                        "" );
 
         assertEquals(623, results.size());
 
